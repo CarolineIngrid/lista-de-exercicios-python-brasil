2943122ac81b2ffee7783f7eb5ea960a0b9d2d83
Ingrid Caroline <ingridcaroline3012@gmail.com>
2022-06-03 11:44:25 -0300

    ex 08 e 09 resolvidos

--- secao_02_estrutura_de_decisao/ex_09_numeros_decrescentes.py
+++ secao_02_estrutura_de_decisao/ex_09_numeros_decrescentes.py
@@ -18,3 +18,7 @@ Faça um Programa que leia três números e mostre-os em ordem decrescente.
 
 def ordenar_decrescente(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+    banana = [x, y, z]
+    banana.sort(reverse=True)
+    print(f"{banana[0]}, {banana[1]}, {banana[2]}")
+
